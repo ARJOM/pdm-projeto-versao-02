c6d4e90eca27ada3bf5f3eb0bd196816d921a716
Antônio Ricart <ricarteusuo11@gmail.com>
2021-05-14 23:12:19 -0300

    env: adiciona estrutura basica do node

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import App from './app';
+import DotEnv from 'dotenv';
+
+DotEnv.config()
+
+const { PORT } = process.env as any;
+
+class Server {
+
+    static start(){
+        new App({ PORT });
+    }
+}
+
+Server.start();
